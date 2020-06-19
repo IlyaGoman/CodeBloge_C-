d96f8ca70e98fc0baa7b9fc106004b68f33cdd3e
goman_iv <realtt@mail.ru>
2020-06-19 09:16:14 +0300

    UDP project

--- ServerTCP/ServerTCP/Program.cs
+++ ServerTCP/ServerTCP/Program.cs
@@ -12,37 +12,71 @@ namespace ServerTCP
     {
         static void Main(string[] args)
         {
+
             const string ip = "127.0.0.1";
-            const int port = 8080;
+            const int port = 8081;
 
-            var tcpEnDPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            #region TCP
+            //var tcpEnDPoint = new IPEndPoint(IPAddress.Parse(ip), port);
 
-            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
-            tcpSocket.Bind(tcpEnDPoint);
-            tcpSocket.Listen(5);
+            //tcpSocket.Bind(tcpEnDPoint);
+            //tcpSocket.Listen(5);
 
-            while(true)
+            //while(true)
+            //{
+            //    var listener = tcpSocket.Accept();
+            //    var buffer = new byte[256];
+            //    var size = 0;
+            //    var strBuider = new StringBuilder();
+
+            //    do
+            //    {
+            //        size = listener.Receive(buffer);
+            //        strBuider.Append(Encoding.UTF8.GetString(buffer, 0, size));
+
+            //    } while (listener.Available>0);
+
+            //    Console.WriteLine(strBuider);
+
+            //    listener.Send(Encoding.UTF8.GetBytes("Successfully"));
+
+            //    listener.Shutdown(SocketShutdown.Both);
+            //    listener.Close();
+            //}
+            #endregion
+
+            #region UDP
+            var udpEnDPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+            var udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            udpSocket.Bind(udpEnDPoint);
+
+            while (true)
             {
-                var listener = tcpSocket.Accept();
                 var buffer = new byte[256];
                 var size = 0;
                 var strBuider = new StringBuilder();
 
+                EndPoint senderEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
                 do
                 {
-                    size = listener.Receive(buffer);
-                    strBuider.Append(Encoding.UTF8.GetString(buffer, 0, size));
+                    size = udpSocket.ReceiveFrom(buffer, ref senderEndPoint);
 
-                } while (listener.Available>0);
+                    strBuider.Append(Encoding.UTF8.GetString(buffer));
+                } while (udpSocket.Available > 0);
+
+                udpSocket.SendTo(Encoding.UTF8.GetBytes("Message receive"), senderEndPoint);
 
                 Console.WriteLine(strBuider);
 
-                listener.Send(Encoding.UTF8.GetBytes("Successfully"));
 
-                listener.Shutdown(SocketShutdown.Both);
-                listener.Close();
             }
+            
+            // TODO: Сделать закрытие сокета
+            #endregion
         }
     }
 }
