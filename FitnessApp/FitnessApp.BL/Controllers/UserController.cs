1c82164e00b513cc2b321df5d6673a2721ef6a52
goman_iv <realtt@mail.ru>
2020-06-24 22:19:26 +0300

    added usercontroller

---
+++ FitnessApp/FitnessApp.BL/Controllers/UserController.cs
@@ -0,0 +1,61 @@
+﻿using FitnessApp.BL.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessApp.BL.Controllers
+{
+    /// <summary>
+    /// Контроллер пользователя.
+    /// </summary>
+    public class UserController
+    {
+        /// <summary>
+        /// Пользователь.
+        /// </summary>
+        public User User { get; }
+
+        /// <summary>
+        /// Создание нового контроллера пользователя.
+        /// </summary>
+        /// <param name="user"> Пользователь. </param>
+        public UserController(User user)
+        {
+            User = user ?? throw new ArgumentNullException("Пользователь не может быть равен NULL.", nameof(user));
+        }
+
+        /// <summary>
+        /// Сохранение пользователя в файл.
+        /// </summary>
+        public void Save()
+        {
+            var binFormatter = new BinaryFormatter();
+
+            using (var file = new FileStream("users.bin", FileMode.OpenOrCreate))
+            {
+                binFormatter.Serialize(file, User);
+            }
+        }
+
+        /// <summary>
+        /// Получить пользователя из файла.
+        /// </summary>
+        /// <returns> Пользователь. </returns>
+        public UserController()
+        {
+            var binFormatter = new BinaryFormatter();
+
+            using (var file = new FileStream("users.bin", FileMode.Open))
+            {
+                User = binFormatter.Deserialize(file) as User;
+            }
+
+            // TODO: Что делать, если пользователя не прочитали?
+        }
+    }
+}
--- FitnessApp/FitnessApp.BL/Model/User.cs
+++ FitnessApp/FitnessApp.BL/Model/User.cs
@@ -2,21 +2,49 @@
 
 namespace FitnessApp.BL.Model
 {
+    /// <summary>
+    /// Пользователь.
+    /// </summary>
     public class User
     {
-
+        #region Свойства
+        /// <summary>
+        /// Имя пользователя.
+        /// </summary>
         public string Name { get; }
+
+        /// <summary>
+        /// Пол пользователя.
+        /// </summary>
         public Gender Gender { get; }
 
+        /// <summary>
+        /// Дата рождения.
+        /// </summary>
         public DateTime BirthDate { get; set; }
 
+        /// <summary>
+        /// Вес пользователя.
+        /// </summary>
         public double Weight { get; set; }
 
+        /// <summary>
+        /// Рост пользователя.
+        /// </summary>
         public double Height { get; set; }
 
+        #endregion
+        /// <summary>
+        /// Создание нового пользователя.
+        /// </summary>
+        /// <param name="name">Имя</param>
+        /// <param name="gender">Пол</param>
+        /// <param name="birthDate">Дата рождения</param>
+        /// <param name="weight">Вес</param>
+        /// <param name="height">Рост</param>
         public User(string name, Gender gender, DateTime birthDate, double weight, double height)
         {
-            #region checked
+            #region Проверка входных параметров
             if (string.IsNullOrWhiteSpace(name))
             {
                 throw new ArgumentNullException("Имя не может быть пустым.", nameof(name));
