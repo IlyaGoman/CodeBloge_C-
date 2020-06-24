f6be08311256e2bbd09d8231d1b07218ceef43c0
goman_iv <realtt@mail.ru>
2020-06-24 15:38:36 +0300

    added classes User and Gender

---
+++ FitnessApp/FitnessApp.BL/Model/Gender.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FitnessApp.BL.Model
+{
+    /// <summary>
+    /// Пол.
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// Наименование пола.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Создание пола
+        /// </summary>
+        /// <param name="name">Наименование</param>
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentNullException("Пол не может быть пустым или null!", nameof(name));
+            Name = name;
+        }
+
+       /// <summary>
+       /// 
+       /// </summary>
+       /// <returns></returns>
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ FitnessApp/FitnessApp.BL/Model/User.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace FitnessApp.BL.Model
+{
+    public class User
+    {
+
+        public string Name { get; }
+        public Gender Gender { get; }
+
+        public DateTime BirthDate { get; set; }
+
+        public double Weight { get; set; }
+
+        public double Height { get; set; }
+
+        public User(string name, Gender gender, DateTime birthDate, double weight, double height)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя не может быть пустым.", nameof(name));
+            }
+
+            if(gender == null)
+            {
+                throw new ArgumentNullException("Пол не может быть пустым.", nameof(gender));
+            }
+
+            if(birthDate <= DateTime.Parse("01.01.1900"))
+            {
+                throw new ArgumentNullException("Дата рождения не может быть меньше 01.01.1900.", nameof(birthDate));
+            }
+
+            if (weight<=0)
+            {
+                throw new ArgumentNullException("Вес не может быть меньше 0.", nameof(weight));
+            }
+
+            if (height<=0)
+            {
+                throw new ArgumentNullException("Рост не может быть меньше 0.", nameof(weight));
+            }
+
+            Name = name;
+            Gender = gender;
+            BirthDate = birthDate;
+            Weight = weight;
+            Height = height;
+        }
+    }
+}
