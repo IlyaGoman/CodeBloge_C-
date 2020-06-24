63fd9c7199b1712d7be8730479ad7cf8646f1bc1
goman_iv <realtt@mail.ru>
2020-06-24 21:26:52 +0300

    added check to User constructor

--- FitnessApp/FitnessApp.BL/Model/User.cs
+++ FitnessApp/FitnessApp.BL/Model/User.cs
@@ -16,36 +16,38 @@ namespace FitnessApp.BL.Model
 
         public User(string name, Gender gender, DateTime birthDate, double weight, double height)
         {
+            #region checked
             if (string.IsNullOrWhiteSpace(name))
             {
                 throw new ArgumentNullException("Имя не может быть пустым.", nameof(name));
             }
 
-            if(gender == null)
+            if (birthDate <= DateTime.Parse("01.01.1900") || birthDate>=DateTime.Now)
             {
-                throw new ArgumentNullException("Пол не может быть пустым.", nameof(gender));
-            }
-
-            if(birthDate <= DateTime.Parse("01.01.1900"))
-            {
-                throw new ArgumentNullException("Дата рождения не может быть меньше 01.01.1900.", nameof(birthDate));
+                throw new ArgumentException("Дата рождения не может быть меньше 01.01.1900.", nameof(birthDate));
             }
 
             if (weight<=0)
             {
-                throw new ArgumentNullException("Вес не может быть меньше 0.", nameof(weight));
+                throw new ArgumentException("Вес не может быть меньше 0.", nameof(weight));
             }
 
             if (height<=0)
             {
-                throw new ArgumentNullException("Рост не может быть меньше 0.", nameof(weight));
+                throw new ArgumentException("Рост не может быть меньше 0.", nameof(height));
             }
+            #endregion
 
             Name = name;
-            Gender = gender;
+            Gender = gender ?? throw new ArgumentNullException("Пол не может быть пустым.", nameof(gender));
             BirthDate = birthDate;
             Weight = weight;
             Height = height;
+        }
+
+        public override string ToString()
+        {
+            return Name;
         }
     }
 }
