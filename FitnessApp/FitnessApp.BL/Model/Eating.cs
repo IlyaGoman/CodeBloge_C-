43277490dfe88228b75636d6214619c597c69ee5
goman_iv <realtt@mail.ru>
2020-06-25 15:49:37 +0300

    added Eating class

---
+++ FitnessApp/FitnessApp.BL/Model/Eating.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FitnessApp.BL.Model
+{
+    /// <summary>
+    /// Прием пищи.
+    /// </summary>
+    public class Eating
+    {
+        public DateTime Moment { get; }
+
+        public List<Food> Foods { get; }
+
+        public User User { get; }
+    }
+}
