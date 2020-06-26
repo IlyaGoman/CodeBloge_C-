905c711d251e925d41d3b98aae97b4b3ac2b767d
goman_iv <realtt@mail.ru>
2020-06-26 08:08:33 +0300

    added EatingControllerTest test on eating/foods

--- FitnessApp/FitnessApp.BLTests/Controllers/EatingControllerTests.cs
+++ FitnessApp/FitnessApp.BLTests/Controllers/EatingControllerTests.cs
@@ -13,21 +13,28 @@ namespace FitnessApp.BL.Controllers.Tests
     public class EatingControllerTests
     {
         [TestMethod()]
-        public void AddTest()
+        public void AddTest_WhenAddedNewFoodIntoEating_ShouldAreEquals()
         {
             //Arrange
+            var rnd = new Random();
             var userName = Guid.NewGuid().ToString();
+            var userGender = Guid.NewGuid().ToString(); ;
+            var userBirthDate = DateTime.Now.AddYears(-20);
+            var userController = new UserController(userName);
+            userController.SetUserData(userGender, userBirthDate);
+
+            var eatingController = new EatingController(userController.CurrentUser);
             var foodName = Guid.NewGuid().ToString();
-            var eatingController = new EatingController(new User(userName));
 
             var addFood = new Food(foodName);
-            var weight = 300;
+            var weight = rnd.Next(10,500);
 
             //Act
             eatingController.Add(addFood, weight);
 
             //Assert
-            Assert.AreEqual(addFood.Name, eatingController.Foods.SingleOrDefault(f=>f.Name==addFood.Name).Name);
+            Assert.AreEqual(addFood.Name, eatingController.Foods.SingleOrDefault(f => f.Name == addFood.Name).Name);
+            Assert.AreEqual(addFood.Name, eatingController.Eating.Foods.Keys.SingleOrDefault(f => f.Name == addFood.Name).Name);
         }
     }
 }
