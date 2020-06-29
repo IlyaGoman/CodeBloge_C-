86d708af9254e05e5ce78e1200f98f6b96ddd694
goman_iv <realtt@mail.ru>
2020-06-29 05:29:32 +0300

    added class activity

---
+++ FitnessApp/FitnessApp.BL/Model/Activity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.Odbc;
+
+namespace FitnessApp.BL.Model
+{
+    public class Activity
+    {
+        /// <summary>
+        /// Наименование активности
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Расход калорий за одну минуту
+        /// </summary>
+        public double CaloriesPerMinute { get; set; }
+
+        public Activity(string name, double caloriesPerMinute)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Наименование активности не может быть пустым.", nameof(name));
+            }
+
+            if (caloriesPerMinute<0)
+            {
+                throw new ArgumentException("Расход калорий не может быть отрицательным.", nameof(caloriesPerMinute));
+            }
+
+            Name = name;
+            CaloriesPerMinute = caloriesPerMinute;
+        }
+    }
+}
