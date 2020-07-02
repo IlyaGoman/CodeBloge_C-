e1997e4c97a2a7000f211339939de947a1d4a5a2
goman_iv <realtt@mail.ru>
2020-07-02 05:29:38 +0300

    work entity to localdb

--- FitnessApp/FitnessApp.BL/Migrations/Configuration.cs
+++ FitnessApp/FitnessApp.BL/Migrations/Configuration.cs
@@ -1,21 +1,22 @@
-﻿using System.Data.Entity.Migrations;
-
-namespace FitnessApp.BL.Migration
+﻿namespace FitnessApp.BL.Migrations
 {
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
     internal sealed class Configuration : DbMigrationsConfiguration<FitnessApp.BL.Controllers.FitnessContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
-            ContextKey = "FitnessApp.BL.Controllers.FitnessContext";
+            AutomaticMigrationsEnabled = false;
         }
-
 
         protected override void Seed(FitnessApp.BL.Controllers.FitnessContext context)
         {
             //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
         }
     }
