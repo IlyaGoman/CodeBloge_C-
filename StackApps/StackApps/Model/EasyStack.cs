8525c080ac14fbdaf28878163ca957b35c4646bd
goman_iv <realtt@mail.ru>
2020-08-26 15:09:47 +0300

    StackApps realised

---
+++ StackApps/StackApps/Model/EasyStack.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StackApps.Model
+{
+    public class EasyStack<T>:IEnumerable
+    {
+        private List<T> items = new List<T>();
+
+        private int Count => items.Count;
+
+        public void Push(T item)
+        {
+            items.Add(item);
+        }
+
+        public T Pop()
+        {
+            if (Count > 0)
+            {
+                var item = items.LastOrDefault();
+                items.RemoveAt(Count-1);
+                return items.LastOrDefault();
+            }
+            else
+                throw new ArgumentNullException(nameof(items));
+        }
+
+        public T Peek()
+        {
+            if (Count > 0)
+            {
+                var item = items.LastOrDefault();
+                return items.LastOrDefault();
+            }
+            else
+                throw new NullReferenceException("Стек пуст");
+        }
+
+        public override string ToString()
+        {
+            return "Stack";
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (var item in items)
+            {
+                yield return item;
+            }
+        }
+    }
+}
--- StackApps/StackApps/Program.cs
+++ StackApps/StackApps/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using StackApps.Model;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,45 @@ namespace StackApps
     {
         static void Main(string[] args)
         {
+            var easyStack = new EasyStack<int>();
+
+            easyStack.Push(1);
+            easyStack.Push(2);
+            easyStack.Push(3);
+            easyStack.Push(4);
+            easyStack.Push(1);
+            easyStack.Push(2);
+
+            foreach (var item in easyStack)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine(easyStack.Pop());
+            Console.WriteLine(easyStack.Peek());
+
+            foreach (var item in easyStack)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+
+
+            Console.ReadLine();
         }
+
+        //public void PrintStack(EasyStack<T> easyStack)
+        //{
+        //    foreach (var item in easyStack)
+        //    {
+        //        Console.Write(item + " ");
+        //    }
+
+        //    Console.WriteLine();
+        //}
+            
     }
 }
