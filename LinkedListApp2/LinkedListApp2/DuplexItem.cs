d5a2d4b0ef2dc9aa51a80f82fa283bea2af2753f
goman_iv <realtt@mail.ru>
2020-09-03 13:26:00 +0300

    added DuplexItem

---
+++ LinkedListApp2/LinkedListApp2/DuplexItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LinkedListApp2
+{
+    public class DuplexItem<T>
+    {
+        public T Data { get; set; }
+
+        public DuplexItem<T> Previous { get; set; }
+        public DuplexItem<T> Next { get; set; }
+
+        public DuplexItem() { }
+
+        public DuplexItem(T data)
+        {
+            Data = data;
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
