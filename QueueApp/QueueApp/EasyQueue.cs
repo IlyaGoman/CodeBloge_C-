e61fcc628a9cd98cc6d2438d2f0e72811a04f8e1
goman_iv <realtt@mail.ru>
2020-09-29 14:54:34 +0300

    Added project QueueApp

---
+++ QueueApp/QueueApp/EasyQueue.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+
+namespace QueueApp
+{
+    public class EasyQueue<T>:IEnumerable
+    {
+        private List<T> items = new List<T>();
+
+        private T Head => items.Last();
+        private T Tail => items.First();
+
+        public int Count => items.Count;
+
+        public EasyQueue()
+        {
+
+        }
+
+        public EasyQueue(T data)
+        {
+            items.Add(data);
+        }
+
+        public void Enqueue(T data)
+        {
+            items.Insert(0, data);
+        }
+
+        public T Dequeue()
+        {
+            var result = Head;
+            
+            if(Count>0)
+                items.RemoveAt(Count - 1);
+
+            return result;
+        }
+
+        public T Peek()
+        {
+            return Head;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            var reverseItems = items;
+            reverseItems.Reverse();
+
+            foreach (var item in reverseItems)
+            {
+                yield return item;
+            }
+        }
+    }
+}
---
+++ QueueApp/QueueApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var easyQueue = new EasyQueue<int>();
+
+            easyQueue.Enqueue(1);
+            easyQueue.Enqueue(2);
+            easyQueue.Enqueue(3);
+            easyQueue.Enqueue(4);
+            easyQueue.Enqueue(5);
+
+            PrintQueue(easyQueue);
+
+            Console.WriteLine(easyQueue.Dequeue());
+            Console.WriteLine(easyQueue.Dequeue());
+            Console.WriteLine(easyQueue.Dequeue());
+            Console.WriteLine(easyQueue.Peek());
+
+            PrintQueue(easyQueue);
+
+            Console.ReadLine();
+
+        }
+
+        static void PrintQueue(EasyQueue<int> queue)
+        {
+            foreach (var item in queue)
+            {
+                Console.Write(item);
+                Console.Write(" ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
