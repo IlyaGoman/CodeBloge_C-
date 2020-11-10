f6f25391beca29ce8398168ce5d744f51f6fc8e3
goman_iv <realtt@mail.ru>
2020-11-10 14:23:43 +0300

    commit for merge

--- SingletonApp/SingletonApp/Program.cs
+++ SingletonApp/SingletonApp/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SingletonApp
 {
@@ -19,15 +18,48 @@ namespace SingletonApp
             //text1.Save();
             //text2.Save();
 
-            var singleton1 = FileWorkerSingleton.Instance;
-            var singleton2 = FileWorkerSingleton.Instance;
+            //var singleton1 = FileWorkerSingleton.Instance;
+            //var singleton2 = FileWorkerSingleton.Instance;
 
-            singleton1.WriteText("Goman");
-            singleton2.WriteText("Ilya");
-            singleton2.WriteText("Bay-Bay");
+            //singleton1.WriteText("Goman");
+            //singleton2.WriteText("Ilya");
+            //singleton2.WriteText("Bay-Bay");
 
-            singleton1.Save();
-            singleton2.Save();
+            //singleton1.Save();
+            //singleton2.Save();
+            
+            foreach (var item in GenerateRange(0,100,5))
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadKey();
+        }
+        //public static List<int> GenerateRange(int min, int max, int step)
+        //{
+        //    List<int> lst = new List<int>();
+        //    for (int i = 0; min + i * step <= max; i++)
+        //    {
+        //        lst.Add(min + i * step);
+        //    }
+        //    return lst;
+        //}
+
+        public static int[] GenerateRange(int min, int max, int step)
+        {
+            int[] result = new int[1+(max - min) / step];
+            for (int i = min; min + i * step <= max; i++)
+            {
+                result[i] = min + i * step;
+            }
+
+            foreach(var item in result.Select(x=>x*(-1)))
+            {
+                Console.Write(item + " ");
+            }
+
+            return result;
         }
     }
+
 }
