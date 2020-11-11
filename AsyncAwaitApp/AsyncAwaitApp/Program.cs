320828e0a3e03e5b646cf7fcd3cb7761189577ff
goman_iv <realtt@mail.ru>
2020-11-11 16:33:12 +0300

    async savetofile

--- AsyncAwaitApp/AsyncAwaitApp/Program.cs
+++ AsyncAwaitApp/AsyncAwaitApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -25,11 +27,43 @@ namespace AsyncAwaitApp
             //} 
             #endregion
 
-            DoWorkAsync();
 
-            Console.WriteLine("Main is working!");
+
+            //var result = SaveToFile("async.txt");
+
+            var result = SaveToFileAsync("async.txt");
+
+            var inputText = Console.ReadLine();
+
+            Console.WriteLine(result.Result);
+
+            Console.WriteLine("Main is completed!");
 
             Console.ReadKey();
+        }
+
+        static async Task<bool> SaveToFileAsync(string path)
+        {
+            var result = await Task<bool>.Run(() => SaveToFile(path));
+
+            Console.WriteLine("SaveToFileAsync completed!");
+            return result;
+        }
+        static bool SaveToFile(string path)
+        {
+            var rnd = new Random();
+            var text = "";
+            for (int i = 0; i < 1000; i++)
+            {
+                text += rnd.Next(0, i);
+            }
+
+            using (StreamWriter sw = new StreamWriter(path, false, Encoding.UTF8))
+            {
+                sw.WriteLine(text);
+            }
+
+            return true;
         }
 
         static async Task DoWorkAsync()
