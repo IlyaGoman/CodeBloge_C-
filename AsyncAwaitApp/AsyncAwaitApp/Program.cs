1e86f2099a188f65ac3e16ba652dcfdaf2b9b024
goman_iv <realtt@mail.ru>
2020-11-11 15:26:44 +0300

    added async method

--- AsyncAwaitApp/AsyncAwaitApp/Program.cs
+++ AsyncAwaitApp/AsyncAwaitApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace AsyncAwaitApp
 {
@@ -6,6 +8,61 @@ namespace AsyncAwaitApp
     {
         static void Main(string[] args)
         {
+            #region Thread
+            //Thread thread = new Thread(new ThreadStart(DoWork));
+            //thread.Start();
+
+            //Thread thread2 = new Thread(new ParameterizedThreadStart(DoWork));
+            //thread2.Start(int.MaxValue);
+
+            //int j = 0;
+            //for (int i = 0; i < int.MaxValue; i++)
+            //{
+            //    j++;
+
+            //    if (i % 100000000 == 0)
+            //        Console.WriteLine("Main");
+            //} 
+            #endregion
+
+            DoWorkAsync();
+
+            Console.WriteLine("Main is working!");
+
+            Console.ReadKey();
+        }
+
+        static async Task DoWorkAsync()
+        {
+            await Task.Run(() => DoWork());
+            Console.WriteLine("DoWorkAsync completed!");
+        }
+
+        static void DoWork()
+        {
+            int j = 0;
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                j++;
+
+                if(j % 100000000 == 0)
+                    Console.WriteLine("DoWork()");
+            }
+        }
+
+        static void DoWork(object max)
+        {
+            if (max is int)
+            {
+                int j = 0;
+                for (int i = 0; i < (int)max; i++)
+                {
+                    j++;
+
+                    if (j % 100000000 == 0)
+                        Console.WriteLine($"DoWork({i})");
+                }
+            }
         }
     }
 }
